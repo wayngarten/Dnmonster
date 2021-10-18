12a0cbf2e54c5c274a4f5172eb34dc6ced02c99d
lexema8_test <lexema@lexema8test.burintekh.local>
2021-10-18 10:16:50 +0000

    add tests

--- cmd.sh
+++ cmd.sh
@@ -5,6 +5,9 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
 	echo "Running Development Server" # Запуск сервера для разработки
 	exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+	echo "Running Unit Tests"
+	exec python "test.py"
 else
 	echo "Running Product Server"  # Запуск сервера для эксплуатации
 	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
